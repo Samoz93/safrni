2b551e367dfead8d5f8fe487a449f8b21b591549
ibrahim alazzawi <nullbytesoftware@gmail.com>
2021-05-09 15:11:00 +0300

    fixed tour info apollo error

--- src/app/data/resolvers/tour-information.resolver.ts
+++ src/app/data/resolvers/tour-information.resolver.ts
@@ -12,6 +12,7 @@ import { TripService } from '../services/trip.service';
 import { CityModel } from '../models/CityModel';
 import { TripModel } from '../models/TripModel';
 import { delay } from '../utils/helpers';
+import { TimelineModel } from '../models/timelineModel';
 @Injectable({ providedIn: 'root' })
 export class TourInformationResolver implements Resolve<any> {
   constructor(
@@ -21,14 +22,15 @@ export class TourInformationResolver implements Resolve<any> {
   async resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Promise<{ trip: TripModel }> {
+  ): Promise<{ trip: TripModel; timelines: TimelineModel[] }> {
     this.splashScreenStateService.start();
     let tripId = route.paramMap.get('id');
     let trip = await this.tripService.getTripById(tripId!);
-
+    let timelines = await this.tripService.getTimeline(trip.timelineId);
     this.splashScreenStateService.stop();
     return {
       trip: trip,
+      timelines: timelines!,
     };
   }
 }
