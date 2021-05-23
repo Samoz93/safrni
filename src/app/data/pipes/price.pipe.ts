76656b8d1a089a3d8e2f95132aac140d5974b967
ibrahim alazzawi <nullbytesoftware@gmail.com>
2021-05-23 21:04:39 +0300

    saferni image overlay type

--- src/app/data/pipes/price.pipe.ts
+++ src/app/data/pipes/price.pipe.ts
@@ -4,13 +4,10 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'price',
 })
 export class PricePipe implements PipeTransform {
-  // transform(pr: string, ...args: unknown[]): unknown {
-  //   if (!pr) return '';
-  //   const sign = signs.filter((f) => f.id == pr.currency)[0]?.sign ?? '$';
-  //   return `${pr?.basePrice ?? 0} ${sign}`;
-  // }
-  transform(pr: string | null, ...args: unknown[]): unknown {
-    return `${pr}$`;
+ 
+  transform(pr: string | number | null, ...args: unknown[]): unknown {
+    
+    return `${pr?.toString()}$`;
   }
 }
 
