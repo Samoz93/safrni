bc74e534f95dc31d3f147405f265b228eab3b47c
ibrahim alazzawi <nullbytesoftware@gmail.com>
2021-05-25 22:27:44 +0300

    pills info

--- src/app/data/pipes/price.pipe.ts
+++ src/app/data/pipes/price.pipe.ts
@@ -4,13 +4,6 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'price',
 })
 export class PricePipe implements PipeTransform {
-  // transform(pr: any | null, ...args: any[]): string {
-  //   const countObj = args[0];
-  //   const adultCount = countObj.adult;
-  //   const childern = countObj.children;
-  //   const cur = countObj.currency;
-  //   return `${pr + adultCount * 10}$ ${cur}`;
-  // }
   transform(pr: string | number | null, ...args: unknown[]): string {
     return `${pr?.toString()}$`;
   }
