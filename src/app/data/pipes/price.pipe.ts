2c151b7a11857035d30b29c9bf43fb99062045ed
ibrahim alazzawi <nullbytesoftware@gmail.com>
2021-05-26 01:13:36 +0300

    fixed trip card info

--- src/app/data/pipes/price.pipe.ts
+++ src/app/data/pipes/price.pipe.ts
@@ -15,16 +15,17 @@ export class PricePipe implements PipeTransform {
   // }
   constructor(private loc: LocalService) {}
   transform(pr: priceData | string, ...args: any[]): string {
-    let basePrice;
-    let cur;
-    if (typeof pr == 'string') {
-      basePrice = pr;
-      cur = args[0];
-    } else {
-      basePrice = pr?.basePrice;
-      cur = pr?.currency;
-    }
+    // let basePrice;
+    // let cur;
+    // if (typeof pr == 'string') {
+    //   basePrice = pr;
+    //   cur = args[0];
+    // } else {
+    //   basePrice = pr?.basePrice;
+    //   cur = pr?.currency;
+    // }
 
-    return `${basePrice}${this.loc.getTranslation(`currencies.${cur}`)}`;
+    // return `${basePrice}${this.loc.getTranslation(`currencies.${cur}`)}`;
+    return `${pr}$`;
   }
 }
